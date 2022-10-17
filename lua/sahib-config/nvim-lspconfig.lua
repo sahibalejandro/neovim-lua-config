240ec391e6ba7108f5fe08a66d4c395b61142fb5
sahib <sahib.alejandro@gmail.com>
2022-10-16 20:23:26 -0500

    Adding keymaps for LSP stuff

--- lua/sahib-config/nvim-lspconfig.lua
+++ lua/sahib-config/nvim-lspconfig.lua
@@ -7,5 +7,18 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
+-- These are the only key mappings defined outside mappings.lua
+local on_attach = function (client, buffer)
+  local opts = { buffer = buffer }
+  vim.keymap.set('n', '<leader>ls', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', '<leader>lR', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>lR', vim.lsp.buf.type_definition, opts)
+end
+
 -- TypeScript (js, jsx, ts, tsx)
-require('lspconfig').tsserver.setup({ capabilities });
+require('lspconfig').tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+});
