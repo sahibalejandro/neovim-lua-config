7597e57abe2de486df0761864f7838e5038e8ead
Sahib <sahib.jaramillo@improving.com>
2022-10-18 15:33:08 -0500

    Added telescope oldfiles picker to key mappings

--- lua/sahib-config/mappings.lua
+++ lua/sahib-config/mappings.lua
@@ -1,12 +1,17 @@
-vim.keymap.set('i', 'jk', '<esc>')
+-- Note:
+-- More mappings are defined in nvim-lspconfig.lua file since they are
+-- part of the LSP configurations, make sure you check those mappings
+-- before adding new ones here to avoid collisions.
 
+vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader><leader>', ':b#<cr>')
 
 -- Telescope
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>o', builtin.find_files)
-vim.keymap.set('n', '<leader>p', builtin.buffers)
+vim.keymap.set('n', '<leader>p', builtin.oldfiles)
 vim.keymap.set('n', '<leader>s', builtin.live_grep)
+vim.keymap.set('n', '<leader>b', builtin.buffers)
 
 -- Nvim Tree
 vim.keymap.set('n', '<leader>1', ':NvimTreeToggle<cr>')
