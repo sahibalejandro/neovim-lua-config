2c71005da7ccb5e18758ccaff009f0917ff909b0
Sahib <sahib.jaramillo@improving.com>
2022-10-18 00:04:24 -0500

    Adding mapping for telescope live_grep

--- lua/sahib-config/mappings.lua
+++ lua/sahib-config/mappings.lua
@@ -3,9 +3,10 @@ vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader><leader>', ':b#<cr>')
 
 -- Telescope
-local telescope = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>o', telescope.find_files)
-vim.keymap.set('n', '<leader>p', telescope.buffers)
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>o', builtin.find_files)
+vim.keymap.set('n', '<leader>p', builtin.buffers)
+vim.keymap.set('n', '<leader>s', builtin.live_grep)
 
 -- Nvim Tree
 vim.keymap.set('n', '<leader>1', ':NvimTreeToggle<cr>')
