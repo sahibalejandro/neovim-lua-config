d8bf07ccea3aefce171ad6195482cc00b6da9732
sahib <sahib.alejandro@gmail.com>
2022-11-04 23:13:14 -0600

    Add key map to format code <leader>lf

--- lua/sahib-config/nvim-lspconfig.lua
+++ lua/sahib-config/nvim-lspconfig.lua
@@ -10,11 +10,17 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(
 -- These are the only key mappings defined outside mappings.lua
 local on_attach = function (client, buffer)
   local opts = { buffer = buffer }
-  vim.keymap.set('n', '<leader>ls', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, opts)
+  -- Displays hover information about the symbol under the cursor in a floating window.
+  vim.keymap.set('n', '<leader>ls', vim.lsp.buf.hover, opts)
+  -- Jumps to the definition of the symbol under the cursor.
   vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, opts)
+  -- Lists all the references to the symbol under the cursor in the quickfix window.
   vim.keymap.set('n', '<leader>lR', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', '<leader>lR', vim.lsp.buf.type_definition, opts)
+  -- Jumps to the definition of the type of the symbol under the cursor.
+  vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, opts)
+
   vim.keymap.set('n', '<leader>li', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '<leader>ln', vim.diagnostic.goto_next, opts)
   vim.keymap.set('n', '<leader>lp', vim.diagnostic.goto_prev, opts)
