fe9dadcfd36522a25e53df63207429c9e6ce9117
sahib <sahib.alejandro@gmail.com>
2023-02-11 00:37:41 -0600

    Center keymap tracker window and format keymaps list

--- lua/plugins/keymap-tracker.lua
+++ lua/plugins/keymap-tracker.lua
@@ -17,20 +17,33 @@ function M.open()
   local buf = vim.api.nvim_create_buf(false, true)
 
   -- todo: calculate the longest keymap to know in wich column the descriptions will be printed.
-  local buf_lines = { 'Press q to exit', '' }
+  local buf_lines = { 'List of tracked keymaps - Press q to close', '' }
+  local max_keymap_len = 0
+
   for i,keymap in ipairs(keymaps) do
-    buf_lines[#buf_lines + 1] = keymap.mode .." " .. keymap.keymap .. " - " .. keymap.description
+    local keymap_len = string.len(keymap.keymap)
+    max_keymap_len = keymap_len > max_keymap_len and keymap_len or max_keymap_len
   end
 
+  for i,keymap in ipairs(keymaps) do
+    local spacing = string.rep(' ', max_keymap_len - string.len(keymap.keymap) + 2)
+    buf_lines[#buf_lines + 1] = " " .. keymap.mode .. "  " .. keymap.keymap .. spacing .. keymap.description
+  end
+
+  -- Calculate the width, height and positon of the
+  -- window to place it in the center of the screen.
+  local win_width = math.floor(vim.o.columns * 0.6)
+  local win_height = math.floor(vim.o.lines * 0.6)
+  local win_col = math.floor(vim.o.columns / 2 - win_width / 2)
+
   local win = vim.api.nvim_open_win(buf, false, {
-    -- todo: center the floating window on the screen
-    col = 0,
-    row = 0,
-    width = 80,
+    row = 2,
+    col = win_col,
     style = 'minimal',
     border = 'single',
+    width = win_width,
+    height = win_height,
     relative = 'editor',
-    height = #buf_lines + 1,
   })
 
   vim.api.nvim_set_current_win(win)
