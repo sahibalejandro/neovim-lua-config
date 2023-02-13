81a4a5b31c55a67e97992e3e914a3bdebebac1b2
sahib <sahib.alejandro@gmail.com>
2023-02-13 00:34:42 -0600

    Open packer messages in a floating window

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,75 +1,82 @@
-return require('packer').startup(function (use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
+return require('packer').startup({
+  function (use)
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/plenary.nvim'
 
-  use 'folke/tokyonight.nvim'
-  use 'nvim-lualine/lualine.nvim'
-  use 'lewis6991/gitsigns.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'nvim-lualine/lualine.nvim'
+    use 'lewis6991/gitsigns.nvim'
 
-  use {
-    'folke/trouble.nvim',
-    config = function()
-      require('trouble').setup()
-    end,
-  }
+    use {
+      'folke/trouble.nvim',
+      config = function()
+        require('trouble').setup()
+      end,
+    }
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-  }
+    use {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.1',
+    }
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  }
-  
-  use {
-    'nvim-tree/nvim-tree.lua',
-    'nvim-tree/nvim-web-devicons',
-  }
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate',
+    }
 
-  use {
-    'mattn/emmet-vim',
-    config = function()
-      vim.g.user_emmet_leader_key = '<c-z>'
-    end,
-  }
+    use {
+      'nvim-tree/nvim-tree.lua',
+      'nvim-tree/nvim-web-devicons',
+    }
 
-  use {
-    'williamboman/mason.nvim',
+    use {
+      'mattn/emmet-vim',
+      config = function()
+        vim.g.user_emmet_leader_key = '<c-z>'
+      end,
+    }
 
-    -- nvim-lspconfig will enable lsp configurations for neovim
-    -- mason-lspconfig is the bridge between mason.nvim and nvim-lspconfig
-    'neovim/nvim-lspconfig',
-    'williamboman/mason-lspconfig.nvim',
+    use {
+      'williamboman/mason.nvim',
 
-    -- null-ls will enable diagnostics from linters and code formatters
-    -- mason-null-ls is the bridge between mason.nvim and null-ls
-    'jose-elias-alvarez/null-ls.nvim',
-    'jay-babu/mason-null-ls.nvim',
-  }
+      -- nvim-lspconfig will enable lsp configurations for neovim
+      -- mason-lspconfig is the bridge between mason.nvim and nvim-lspconfig
+      'neovim/nvim-lspconfig',
+      'williamboman/mason-lspconfig.nvim',
 
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-      'SirVer/ultisnips',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
+      -- null-ls will enable diagnostics from linters and code formatters
+      -- mason-null-ls is the bridge between mason.nvim and null-ls
+      'jose-elias-alvarez/null-ls.nvim',
+      'jay-babu/mason-null-ls.nvim',
+    }
+
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        'SirVer/ultisnips',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+      },
+    }
+
+    use {
+      '~/.config/nvim/lua/plugins/keymap-tracker.nvim',
+      config = function()
+        require('keymap-tracker').setup()
+      end,
+    }
+
+    use {
+      '~/.config/nvim/lua/plugins/lazygit.nvim',
+      config = function()
+        require('lazygit').setup()
+      end,
+    }
+  end,
+  config = {
+    display = {
+      open_fn = require('packer.util').float,
     },
-  }
-
-  use {
-    '~/.config/nvim/lua/plugins/keymap-tracker.nvim',
-    config = function()
-      require('keymap-tracker').setup()
-    end,
-  }
-
-  use {
-    '~/.config/nvim/lua/plugins/lazygit.nvim',
-    config = function()
-      require('lazygit').setup()
-    end,
-  }
-end)
+  },
+})
