5fa0f8259a1251a643dca444404baef8e1830914
sahib <sahib.alejandro@gmail.com>
2023-02-13 00:35:48 -0600

    Enabling splitright and splitbelow

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,6 +8,8 @@ vim.o.signcolumn = 'yes'
 vim.o.swapfile = false
 vim.o.scrolloff = 5
 vim.o.sidescrolloff = vim.o.scrolloff
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- (Nvim Tree) Disable netrw: https://github.com/nvim-tree/nvim-tree.lua#setup
 vim.g.loaded_netrw = 1
