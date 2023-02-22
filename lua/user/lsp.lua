ec1cc0655368e7fbcd232340332650285e58d604
sahib <sahib.alejandro@gmail.com>
2023-02-21 23:22:32 -0600

    Adding lua_ls LSP configuration

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -6,21 +6,24 @@ local mappings = require('user.mappings')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
 -- General options for all servers
-local server_options = {
-  on_attach = function (client, buffer)
+local common_options = {
+  on_attach = function (_, buffer)
     mappings.lsp_mappings({ buffer = buffer })
   end,
   capabilities = cmp_nvim_lsp.default_capabilities()
 }
 
 -- Specific options for TypeScript
-local typescript_options = {
-  on_attach = server_options.on_attach,
-  capabilities = server_options.capabilities,
-  -- Disable single file support to make sure the server is only
-  -- attached when root_dir does not return nil.
+local tsserver_options = {
+  on_attach = common_options.on_attach,
+  capabilities = common_options.capabilities,
+
+  -- Disable single file support to ensure the server is
+  -- attached only when root_dir() does not return nil.
   single_file_support = false,
+
   root_dir = function(filepath)
+    -- Disable tsserver if the current project is using Flow
     if lsp.util.root_pattern('.flowconfig')(filepath) then
       return nil
     end
@@ -29,7 +32,8 @@ local typescript_options = {
   end
 }
 
-lsp.flow.setup(server_options)
-lsp.volar.setup(server_options)
-lsp.intelephense.setup(server_options)
-lsp.tsserver.setup(typescript_options)
+lsp.flow.setup(common_options)
+lsp.volar.setup(common_options)
+lsp.lua_ls.setup(common_options)
+lsp.tsserver.setup(tsserver_options)
+lsp.intelephense.setup(common_options)
