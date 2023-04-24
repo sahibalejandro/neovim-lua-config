5ee542176a9349eb40614cfea85209ba2a35ad06
Sahib <sahib.jaramillo@improving.com>
2023-04-24 12:48:35 -0600

    Added keymap for Telescope grep_string

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,16 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
 
   keys = {
+    -- Lists files in your current directory.
     { "<leader>o", "<cmd>Telescope find_files<cr>" },
+
+    -- Lists previously open files.
     { "<leader>p", "<cmd>Telescope oldfiles<cr>" },
+
+    -- Search for a string in your current working directory.
     { "<leader>s", "<cmd>Telescope live_grep<cr>" },
+
+    -- Searches for the string under the cursor or selection.
+    { "<leader>k", "<cmd>Telescope grep_string<cr>"},
   },
 }
