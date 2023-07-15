9c0a277d774e42f77b7ab97fb47313ad514a90dd
Sahib <sahib.alejandro@gmail.com>
2023-07-15 16:52:20 -0600

    Adding CMP capabilities to LSP servers

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -9,7 +9,11 @@ return {
     require("mason-lspconfig").setup()
     require("mason-lspconfig").setup_handlers({
       function (server_name)
-        require("lspconfig")[server_name].setup({})
+        -- Add CMP capabilities to LSP servers.
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities
+        })
       end,
     })
   end,
