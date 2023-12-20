8a038ee1f075b908761004a00c6d33f3e03bee2d
Sahib Jaramillo <sahib.jaramillo@improving.com>
2023-12-19 20:03:37 -0600

    Adding keymaps to vertically resize windows

--- lua/bootstrap/neovim.lua
+++ lua/bootstrap/neovim.lua
@@ -22,6 +22,8 @@ vim.o.hlsearch = false
 -- Useful keymaps
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader><leader>", ":b#<cr>")
+vim.keymap.set("n", "<right>", ":vert res +10<cr>")
+vim.keymap.set("n", "<left>", ":vert res -10<cr>")
 
 -- Sort visual selected lines by length
 vim.keymap.set("v", "<leader>s", ":!awk '{ print length(), $0 | \"sort -n | cut -d\\\\  -f2-\" }'<cr>")
