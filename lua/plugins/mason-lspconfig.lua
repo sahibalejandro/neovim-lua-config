caf02c9d17f671bdd1b4541bd8e75d1896b9267f
Sahib Jaramillo <sahib.jaramillo@improving.com>
2024-01-04 10:16:54 -0600

    Adding intelephense to LSP servers

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -13,6 +13,10 @@ return {
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+    lspconfig.intelephense.setup({
+      capabilities = capabilities,
+    })
+
     lspconfig.tsserver.setup({
       capabilities = capabilities,
     })
